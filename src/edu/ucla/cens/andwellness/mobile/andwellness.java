4ebace3e4b9829c54c4e177986ab4fd956116801
Michael Au-Yeung <michaelay@gmail.com>
2011-05-12 12:44:56 -0700

    First commit

---
+++ src/edu/ucla/cens/andwellness/mobile/andwellness.java
@@ -0,0 +1,15 @@
+package edu.ucla.cens.andwellness.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class andwellness extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
